cb5b32478a0714bcdb4747d7f33b3fb4613649aa
odidus <aglomerat@gmail.com>
2019-04-05 14:06:32 +0300

    prima comitere la varianta hash de rutare

--- src/router.js
+++ src/router.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
+import vItemX from './views/ItemDetails.vue'
+
 
 Vue.use(Router)
 
@@ -12,11 +14,13 @@ export default new Router({
       component: Home
     },
     {
+      path: '/itemx',
+      name: 'itemx',
+      component: vItemX
+    },
+    {
       path: '/about',
       name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
       component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
     }
   ]
